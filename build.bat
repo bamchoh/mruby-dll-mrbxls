a7c06482de2979798c28b2af0c044cab95c47dd3
bamchoh <bamchoh@gmail.com>
2014-02-23 22:46:03 +0900

    add build_config

---
+++ build.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+set MRUBY_DLL_DIR=%cd%
+git submodule init
+set MRUBY_CONFIG=%cd%\build_config.rb
+set MRUBY_RAKEFILE=%cd%\Rakefile2
+cd mruby
+ruby minirake -f %MRUBY_RAKEFILE% clean
+ruby minirake -f %MRUBY_RAKEFILE% dll
+endlocal
